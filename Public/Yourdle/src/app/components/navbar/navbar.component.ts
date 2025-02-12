8cf975dc392dac85ac52055f636a233b4afc4e3c
bartekaron <78853724+bartekaron@users.noreply.github.com>
2025-02-12 13:40:12 +0100

    Navbar első verziójának elkészítése

---
+++ Public/Yourdle/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [Menubar, CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Egyjátékos',
+        icon: 'pi pi-fw pi-info',
+        routerLink: '/egyjatekos'
+      },
+      {
+        label: 'Toplista',
+        icon: 'pi pi-fw pi-envelope',
+        routerLink: '/toplista'
+      },
+      {
+        label: 'Bejelentkezés',
+        icon: 'pi pi-fw pi-power-off',
+        routerLink: '/bejelentkezes'
+      }
+    ];
+  }
+
+}
