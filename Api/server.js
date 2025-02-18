99f4fef3720536c5c058a85de2e107f19c03caa3
vargajanos <janosvarga706@gmail.com>
2025-02-18 13:13:50 +0100

    YOUR-59 Kép feltöltése sikeres csak max 64KB, majd a csapat konzultálás után lehet változás lesz

--- Api/server.js
+++ Api/server.js
@@ -22,8 +22,6 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 app.post('/uploadProfilePicture', upload.single('profilePicture'), async (req, res) => {
-    console.log("Received file:", req.file);  // Ellenőrizzük, hogy a fájl megérkezik
-    console.log("Received body:", req.body);  // Ellenőrizzük, hogy az ID megérkezik
 
     if (!req.file) {
         return res.status(400).json({ error: "No file uploaded" });
@@ -37,18 +35,17 @@ app.post('/uploadProfilePicture', upload.single('profilePicture'), async (req, r
     const id = req.body.id;
 
     try {
-        const result = await pool.query(
-            'UPDATE users SET profilePic = $1 WHERE id = $2 RETURNING id, profilePic',
-            [fileBuffer, id]
+        await pool.query(
+            'UPDATE users SET profilePic = ? WHERE id = ?', [fileBuffer, id], (err, results)=>{
+                if (err) {
+                    return res.status(500).json({error: "Hiba az adatbázisban"})
+                }
+                if (results.length === 0) {
+                    return res.status(400).json({error: "Fos"});
+                }
+                res.status(200).json(results);
+            }
         );
-
-        console.log("SQL Query Result:", result);
-
-        if (!result || !result.rows || result.rows.length === 0) {
-            return res.status(404).json({ error: "User not found or update failed" });
-        }
-
-        res.status(200).json(result.rows[0]);
     } catch (err) {
         console.error("Database error:", err);
         res.status(500).json({ error: err.message });
